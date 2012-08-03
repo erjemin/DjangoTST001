77d04ba08eaa7e8d40cbbdc082ce7227319819b1
erjemin <erjemin@rambler.ru>
2012-08-03 18:08:49 +0400

    Проба!

---
+++ DjangoTST001/urls.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Включили поддержку UTF-8 в Python. Без этого даже комментарии на русском языке нельзя писать.
+
+from django.conf.urls import *
+# from django.conf.urls import patterns, include, url
+
+from DjangoTST001.views import hello
+from DjangoTST001.views import funMainHomePage
+from DjangoTST001.views import hours_ahead
+from DjangoTST001.views import junk_nav
+
+# Расскоментить следующие две строки для активации интерфейса администратора
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    ( r'^hello/$', hello ),
+    ( r'^time/$', funMainHomePage ),
+    ( r'^time/plus/(\d{1,2})/$', hours_ahead ),
+    ( r'^nav/(\d{1,4})/$', junk_nav ),
+    # Пример:
+    # url(r'^$', 'DjangoTST001.views.home', name='home'),
+    # url(r'^DjangoTST001/', include('DjangoTST001.foo.urls')),
+
+    # Раскоментить строку admin/doc ниже для активации документации по администрированию:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Расскоментить следующую строку для активации интерфейса администратора:
+    url(r'^admin/', include(admin.site.urls)),
+)
---
+++ DjangoTST001/views.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+# Включили поддержку UTF-8 в Python. Без этого даже комментарии на русском языке нельзя писать.
+
+from django.http import HttpResponse, Http404
+import random
+import datetime
+import time
+
+
+def hello ( request ) :
+    return HttpResponse ( "Hello world! Привет питон!")
+
+def funMainHomePage ( request ) :
+    szHello = ""
+    for  szCount in u'Главная страница' :
+        szHello += szCount*random.randint(1, 3)
+    szHello = '<html><body><h1>' + szHello                 \
+              + '</h1><h2> %s </h2></body></html>'         \
+              % datetime.datetime.now( )
+    return HttpResponse ( szHello )
+
+def hours_ahead ( request, offset ) :
+    try:
+        offset = int ( offset )
+    except ValueError:
+        raise Http404 ( )
+    dt = datetime.datetime.now ( ) + datetime.timedelta( hours = offset )
+    html = "<html><body>Через %s часов будет %s.</body></html>"     \
+           % ( offset, dt )
+    return HttpResponse ( html )
+
+def junk_nav ( request, offset ) :
+    tStart = time.clock()
+    random.seed ( offset )
+    dimX = 30  # число столбцов навигационного массива
+    dimY = 10  # число строк навигационного массива
+    iNumNavFocus = random.randint(2,6)   # число очагов навигации
+    iMaxRadiusFocus = 12     # предельный разбег фокуса навигации
+    iMinRadiusFocus = 5     # минимальный разбег фокуса навигации
+    # инициализируем массив размерностью dimX на dimY и глубиной 2
+    dim = [[[ 0 # countY * dimX + countX + 1
+        for i in range(2)]
+            for countY in range(dimY)]
+                for countX in range(dimX)]
+
+    # начинаем заполнять навигационный массив
+    iCountDown = dimX * dimY
+    for CountFocus in range ( iNumNavFocus ) :
+        # перебираем по циклу очаги навигации
+        iRadiusFocus = random.randint(iMinRadiusFocus, iMaxRadiusFocus )
+        iCurentCenterFocusX = random.randint(1,dimX-2)
+        iCurentCenterFocusY = random.randint(1,dimY-2)
+        dim[iCurentCenterFocusX][iCurentCenterFocusY][0] = iCountDown
+        iCountDown -= 1
+        iCurentFocusSquare = 1
+        for CountRadius in range (1, iRadiusFocus ) :
+            iCurentFocusSquare = CountRadius * CountRadius
+            for CountTmpFocus in range ( 1, iCurentFocusSquare*2 ) :
+                iTmp0X = random.randint(iCurentCenterFocusX-CountRadius,iCurentCenterFocusX+CountRadius)
+                iTmp0Y = random.randint(iCurentCenterFocusY-CountRadius,iCurentCenterFocusY+CountRadius)
+                if iTmp0Y < 0 or iTmp0X < 0 or iTmp0Y >= dimY or iTmp0X >= dimX:
+                    continue
+
+                if dim[iTmp0X][iTmp0Y][0] == 0 :
+                    dim[iTmp0X][iTmp0Y][0] = iCountDown
+                    iCountDown -= 1
+
+    # результирующий вывод
+    html = "<html><body><table cellpadding='5' cellspacing='1'>"
+    for CountY in range( dimY ) :
+        html += "<tr>"
+        for CountX in range( dimX ) :
+            html += "<td bgcolor='#"
+            html += "00%02xff" % int((dim[CountX][CountY][0]*255.)/300)
+            html += "'><a href='/nav/%03d/'" % dim[CountX][CountY][0]
+            html += ">%03d</a></td>" % dim[CountX][CountY][0]
+        html += "</tr>"
+    tEnd =  time.clock()
+    html += "</table> Время выполнения: %f " % float(tEnd - tStart)
+    html += "</body></html>"
+
+    return HttpResponse ( html )
